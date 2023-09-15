0de9e047819e6717602134b4f497bf8a7cb72466
Elm2006 <143172014+Elm2006@users.noreply.github.com>
2023-09-15 13:17:09 +0200

    Update Program.cs

--- Uppgift 2.5 in/Program.cs
+++ Uppgift 2.5 in/Program.cs
@@ -7,7 +7,7 @@ namespace Uppgift_2.__5
         {
             Console.WriteLine("Skriv två decimaltecken som du vill multiplacera med varandra genom att bara ha ett gånger tecken mellan talen utan mellanslag..");
             string uttryck= Console.ReadLine();
-            int gånger = uttryck.IndexOf("*");
+            string gånger = uttryck.IndexOf("*");
             string tal1 = uttryck[..gånger];
             string tal2 = uttryck[(gånger + 1)..];
             double pro1 =double.Parse(tal1);

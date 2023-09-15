d2d470cfff2aed8639df61692260ec995d817515
isel06002 <isel06002@E-PW0234HX>
2023-09-15 13:16:12 +0200

    Add project files.

---
+++ Uppgift 2.5 in/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Uppgift_2.__5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv två decimaltecken som du vill multiplacera med varandra genom att bara ha ett gånger tecken mellan talen utan mellanslag..");
+            string uttryck= Console.ReadLine();
+            int gånger = uttryck.IndexOf("*");
+            string tal1 = uttryck[..gånger];
+            string tal2 = uttryck[(gånger + 1)..];
+            double pro1 =double.Parse(tal1);
+            double pro2 =double.Parse(tal2);
+            Console.WriteLine(pro1 * pro2);
+            Console.ReadKey();
+        }
+    }
+}
